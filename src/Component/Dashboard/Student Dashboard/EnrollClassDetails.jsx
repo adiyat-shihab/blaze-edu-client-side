0e0a3c154b041ff74e282549c5389bfba5a8ad82
adiyat-shihab <mohammadshihab104@gmail.com>
2023-11-28 01:21:47 +0600

    Refactored EnrollClassDetails.jsx to import necessary dependencies, add state for currentDate, fetch user details with useEffect, and implement form submission functionality with useMutation and Swal.

--- src/Component/Dashboard/Student Dashboard/EnrollClassDetails.jsx
+++ src/Component/Dashboard/Student Dashboard/EnrollClassDetails.jsx
@@ -1,19 +1,38 @@
 import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useAxiosPrivate } from "../../../Hooks/useAxiosPrivate.jsx";
-import { Table } from "antd";
+import { Button, Table } from "antd";
+import { useEffect, useState } from "react";
+import { UseAuth } from "../../../Hooks/UseAuth.jsx";
+import Swal from "sweetalert2";
 
 export const EnrollClassDetails = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
   const param = useParams();
   const axiosSecure = useAxiosPrivate();
-  console.log(param);
+  const { userDetails } = UseAuth();
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   const { data } = useQuery({
     queryKey: ["studentAssignment", param.id],
     queryFn: async () => {
       return await axiosSecure.get(`/student/assignment/${param.id}`);
     },
   });
-  console.log(data);
+
+  const mutation = useMutation({
+    mutationFn: (data) => {
+      return axiosSecure.post(`/student/assignment/submit`, data);
+    },
+  });
+
   const columns = [
     {
       title: "Assignment Title",
@@ -29,6 +48,49 @@ export const EnrollClassDetails = () => {
       title: "Description",
       dataIndex: "description",
       key: "address",
+    },
+    {
+      render: (record) => {
+        const assignmentDate = new Date(record?.date);
+
+        const formattedCurrentDate = `${
+          currentDate.getMonth() + 1
+        }/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+
+        const formattedCurrentDateObject = new Date(formattedCurrentDate);
+
+        const isBeforeDeadline = formattedCurrentDateObject > assignmentDate;
+        const data = {
+          class_id: record?.class_id,
+          assignment_id: record?._id,
+          submission_date: formattedCurrentDate,
+          student_email: userDetails?.email,
+        };
+        const handleSubmit = (data) => {
+          mutation.mutate(data);
+          if (mutation.data?.data?.message) {
+            Swal.fire({
+              icon: "error",
+              title: mutation.data?.data?.message,
+            });
+          } else if (mutation.data?.data) {
+            Swal.fire({
+              icon: "success",
+              title: "Assignment Submitted Successfully",
+            });
+          }
+        };
+
+        return (
+          <>
+            {isBeforeDeadline ? (
+              <Button disabled>Submit</Button>
+            ) : (
+              <Button onClick={() => handleSubmit(data)}>Submit</Button>
+            )}
+          </>
+        );
+      },
     },
   ];
   return (
